50a52eb205e2a25fe096247ccddb685cb4e56bf4
unknown <sosm8100@gmail.com>
2019-10-09 02:24:20 +0900

    admin_practice

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    # Custom User Admin
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom profile",
+            {
+                "fields": (
+                    "profileimg",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
