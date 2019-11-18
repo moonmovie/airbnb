c6ecaeff99c713f0ebfa50aca34efdf9e7a8dd01
unknown <sosm8100@gmail.com>
2019-11-19 03:16:25 +0900

    room type

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,6 +3,11 @@ from . import models
 
 # Register your models here.
 
+# admin + 추가 할수 있는 기능
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
--- rooms/models.py
+++ rooms/models.py
@@ -8,19 +8,40 @@ from users import models as user_models
 # Create your models here.
 
 
-class Room(core_models.TimeStampedmodel):
-    name = models.CharField(max_length=140)
-    description = models.TextField()
-    country = CountryField()
-    city = models.CharField(max_length=80)
-    price = models.IntegerField()
-    address = models.CharField(max_length=140)
-    guests = models.IntegerField()
-    beds = models.IntegerField()
-    bedsrooms = models.IntegerField()
-    baths = models.IntegerField()
-    check_in = models.TimeField()
-    check_out = models.TimeField()
-    instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+class AbstractItem(core_models.TimeStampedmodel):
 
+    # Abstract Model
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
+
+
+class Room(core_models.TimeStampedmodel):
+    name = models.CharField(max_length=140, null=False, default="")
+    description = models.TextField(null=True)
+    country = CountryField(null=True)
+    city = models.CharField(max_length=80, default="")
+    price = models.IntegerField(null=True)
+    address = models.CharField(max_length=140, default="")
+    guests = models.IntegerField(null=True)
+    beds = models.IntegerField(null=True)
+    bedsrooms = models.IntegerField(null=True)
+    baths = models.IntegerField(null=True)
+    check_in = models.TimeField(null=True)
+    check_out = models.TimeField(null=True)
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE, default="")
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
